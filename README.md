ae616ddec1c6f41ea59f2ad99ad1749954ea87dd
FigoFavian <figo21fr@gmail.com>
2025-05-20 09:17:27 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Bukti:
 
 ![alt text](image.png)
 
+## 1.3 Multiple Spawn and Removing Drop
+
+### Drop On:
+
+Menghilangkan `drop(spawner)` akan menyebabkan program tidak berhenti. , `spawner` akan membuat _task_ baru yang disalurkan ke _task sender_ saat fungsi `spawn` dipanggil. Salah satu _task_ dari _task sender_ akan diambil dan kemudian di eksekusi oleh `Executor`, lalu _task_ yang baru akan diambil sampai habis. `spawner` seharusnya di-_drop_ karena hal tersebut mengindikasikan bahwa interaksi sudah selesai.
+
+Bukti:
+
+![alt text](image-1.png)
+
+### Drop Off:
+
+Namun jika `drop(spawner)` masih ada, maka akan berhenti.
+
+Bukti:
+
+![alt text](image-2.png)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::{
+    use futures::{
     future::{BoxFuture, FutureExt},
     task::{waker_ref, ArcWake},
 };
@@ -106,10 +106,24 @@ fn main() {
         println!("figo's computer: done!");
     });
 
-     println!("figo's computer: hey hey!");
+    spawner.spawn(async {
+        println!("figo's computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("figo's computer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("figo's computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("figo's computer: done3!");
+    });
+
+        println!("figo's computer: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    //drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
